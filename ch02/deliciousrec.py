f7a46b258b26a3f48268ee219289c3eac91a410d
ffs <469234000@qq.com>
2017-04-11 11:28:31 +0800

    gannbare

--- ch02/deliciousrec.py
+++ ch02/deliciousrec.py
@@ -1,8 +1,10 @@
 # coding=utf-8
+import pdb
 from pydelicious import get_popular,get_userposts,get_urlposts
 
 def initializeUserDict(tag,count=5):
 	user_dict={}
+	# pdb.set_trace()
 	# 获取前count个最受欢迎的链接张贴记录
 	for p1 in get_popular(tag=tag)[0:count]:
 		# 查找所有张贴该链接的用户
@@ -10,3 +12,4 @@ def initializeUserDict(tag,count=5):
 			user=p2['user']
 			user_dict[user]={}
 	return user_dict
+
